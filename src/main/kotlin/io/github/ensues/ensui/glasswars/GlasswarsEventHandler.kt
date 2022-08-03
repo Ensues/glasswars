9763f4226d0735e57b855008026b5f56b278d97d
trashoflevillage <uhhnokthxbye@gmail.com>
2022-08-02 22:24:34 -0400

    Glasswars game now utilizes EnsuiWorld

---
+++ src/main/kotlin/io/github/ensues/ensui/glasswars/GlasswarsEventHandler.kt
@@ -0,0 +1,6 @@
+package io.github.ensues.ensui.glasswars
+
+import io.github.ensues.ensui.core.events.EventHandler
+
+class GlasswarsEventHandler : EventHandler {
+}
--- src/main/kotlin/io/github/ensues/ensui/glasswars/GlasswarsGame.kt
+++ src/main/kotlin/io/github/ensues/ensui/glasswars/GlasswarsGame.kt
@@ -1,15 +1,25 @@
 package io.github.ensues.ensui.glasswars
 
 import io.github.ensues.ensui.core.Tickable
+import io.github.ensues.ensui.core.events.EventDriver
+import io.github.ensues.ensui.core.events.EventHandler
+import io.github.ensues.ensui.core.world.EnsuiWorld
 import io.github.ensues.ensui.glasswars.maps.GlasswarsMap
+import io.github.ensues.ensui.glasswars.maps.GlasswarsMapType
 import io.github.ensues.ensui.glasswars.teams.GlasswarsTeam
+import org.bukkit.World
 import org.bukkit.entity.Player
 
-class GlasswarsGame(private val gameMap: GlasswarsMap) : Tickable {
+class GlasswarsGame(world: World, private val gameMapType: GlasswarsMapType) : Tickable, EnsuiWorld(world, GlasswarsEventHandler()) {
+    private val gameMap = gameMapType.getInstance(world)
     private val teams = gameMap.teams.map { GlasswarsTeam(it) }
     private val generators = gameMap.generators
     val phase: GlasswarsPhase = GlasswarsPhase.QUEUE;
     val players = mutableListOf<GlasswarsPlayer>()
+    override fun joinWorld(player: Player) {
+        player.teleport(gameMap.lobbyLocation)
+        players.add(GlasswarsPlayer(player))
+    }
 
     override fun tick() {
         teams.forEach { it.tick() } // Tick all teams
@@ -20,6 +30,5 @@ class GlasswarsGame(private val gameMap: GlasswarsMap) : Tickable {
     fun addPlayer(player: Player) {
         players.add(GlasswarsPlayer(player))
         player.teleport(gameMap.lobbyLocation)
-
     }
 }
